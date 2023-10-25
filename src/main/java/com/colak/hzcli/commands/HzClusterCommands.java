d99b228c57ca6489a3dbe883341e5de28829931e
Orçun Çolak <orcun.colak@hazelcast.com>
2023-10-25 12:22:01 +0300

    show cluster members command

---
+++ src/main/java/com/colak/hzcli/commands/HzClusterCommands.java
@@ -0,0 +1,28 @@
+package com.colak.hzcli.commands;
+
+import com.hazelcast.version.MemberVersion;
+import org.springframework.shell.standard.ShellComponent;
+import org.springframework.shell.standard.ShellMethod;
+
+import java.util.Map;
+
+@ShellComponent
+public class HzClusterCommands extends AbstractCommand {
+
+    @ShellMethod("List Hazelcast cluster members")
+    void members() {
+        embedInTable(
+                new String[]{"member", "attributes", "version"},
+                builder -> hazelcastClient.getCluster().getMembers().forEach(member -> {
+                    builder.addRow();
+                    String str = member.toString();
+                    builder.addValue(str);
+
+                    Map<String, String> attributes = member.getAttributes();
+                    builder.addValue(attributes);
+
+                    MemberVersion version = member.getVersion();
+                    builder.addValue(version);
+                }));
+    }
+}
