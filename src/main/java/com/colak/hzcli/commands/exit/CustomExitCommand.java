da0949e2a0efed1b41b6290d72ba92c4a26acd67
Orçun Çolak <orcun.colak@hazelcast.com>
2023-10-27 12:16:16 +0300

    exit command to shutdown hz client

---
+++ src/main/java/com/colak/hzcli/commands/exit/CustomExitCommand.java
@@ -0,0 +1,21 @@
+package com.colak.hzcli.commands.exit;
+
+import com.colak.hzcli.commands.AbstractCommand;
+import org.springframework.shell.standard.ShellComponent;
+import org.springframework.shell.standard.ShellMethod;
+import org.springframework.shell.standard.commands.Quit;
+
+@ShellComponent
+public class CustomExitCommand extends AbstractCommand implements Quit.Command {
+
+    // From https://self-learning-java-tutorial.blogspot.com/2021/02/spring-shell-override-built-in-commands.html
+    @ShellMethod(value = "Exit the shell.", key = {"quit", "exit", "terminate"})
+    public void quit() {
+        hazelcastClient.shutdown();
+
+        System.out.println("Exiting the Application");
+        System.out.println("Good Bye!!!!!!!!");
+        System.exit(0);
+
+    }
+}
