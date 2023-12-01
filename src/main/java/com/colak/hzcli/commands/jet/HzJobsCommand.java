a450ab8665cbd950beae99c4057d22411b354d1a
Orçun Çolak <orcun.colak@hazelcast.com>
2023-12-01 15:58:42 +0300

    submit jet job jar

--- src/main/java/com/colak/hzcli/commands/jet/HzJobsCommand.java
+++ src/main/java/com/colak/hzcli/commands/jet/HzJobsCommand.java
@@ -3,9 +3,17 @@ package com.colak.hzcli.commands.jet;
 import com.colak.hzcli.commands.AbstractCommand;
 import com.hazelcast.jet.JetService;
 import com.hazelcast.jet.Job;
+import com.hazelcast.jet.impl.JetClientInstanceImpl;
+import com.hazelcast.jet.impl.SubmitJobParameters;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.shell.standard.ShellComponent;
 import org.springframework.shell.standard.ShellMethod;
+import org.springframework.shell.standard.ShellOption;
 
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
@@ -13,14 +21,15 @@ import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 @ShellComponent
+@Slf4j
 public class HzJobsCommand extends AbstractCommand {
 
     @ShellMethod(key = "jet.jobs", value = "List all Jet jobs")
-    void showExecutorStats() {
+    void showJetJobs() {
         JetService jetService = hazelcastClient.getJet();
         List<Job> jobs = jetService.getJobs();
         embedInTable(
-                new String[]{"name", "light job", "id", "status", "submission time" , "job config"},
+                new String[]{"name", "light job", "id", "status", "submission time", "job config"},
                 builder -> jobs
                         .forEach(job -> {
                             builder.addRow();
@@ -34,6 +43,29 @@ public class HzJobsCommand extends AbstractCommand {
 
                             builder.addValue(job.getConfig());
                         }));
+    }
+
+    // jet.submitjar "D:/Users/user/.m2/com/hazelcast/jet/tests/kafka-connect-source/5.4.0-SNAPSHOT/kafka-connect-source-5.4.0-SNAPSHOT.jar"
+    @ShellMethod(key = "jet.submitjar", value = "Submit Jet Job jar")
+    void submitJar(@ShellOption @Valid @NotNull String filePath) {
+        try {
+            Path jarPath = Paths.get(filePath);
+
+            JetClientInstanceImpl jetClientInstance = getClientJetService();
+            SubmitJobParameters submitJobParameters = SubmitJobParameters.withJarOnClient()
+                    .setJarPath(jarPath);
+
+            jetClientInstance.submitJobFromJar(submitJobParameters);
+
+            shellHelper.printSuccess("Success");
+        } catch (Exception exception) {
+            log.error("Sql Exception: {}", exception.getMessage(), exception);
+            shellHelper.printError("Failed");
+        }
+    }
+
+    private JetClientInstanceImpl getClientJetService() {
+        return (JetClientInstanceImpl) hazelcastClient.getJet();
     }
 
     private String formatInstant(long time) {
